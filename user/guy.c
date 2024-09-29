1bfb66786afc21cbf3673c950860427a5abfa4d4
Eva Grace DeThomas <egdethomas@gojira.cs.usfca.edu>
2024-09-28 23:42:20 -0700

    Guy running partially implemented

--- user/guy.c
+++ user/guy.c
@@ -2,13 +2,39 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-void printGuy() {
+void clearScreen() {
+	printf("\033[H\033[J");
+}
+
+void printGuy(int pos) {
+	for (int j = 0; j < pos; j++) {
+        	printf(" ");
+	}
+
 	printf("   O   \n");
-    	printf("  /|\\  \n");
-    	printf("  / \\  \n");
+    	
+	for (int j = 0; j < pos; j++) {
+                printf(" ");
+        }
+	printf("  /|\\  \n");
+    	
+	for (int j = 0; j < pos; j++) {
+                printf(" ");
+        }
+	printf("  / \\  \n");
+}
+
+void guyRun() {
+	for (int i = 0; i < 30; i++) {
+        	clearScreen();
+       		printGuy(i);
+        	sleep(5);
+    	}
 }
 
 int main () {
-	printGuy();
+
+	guyRun();
+
 	return 0;
 }
